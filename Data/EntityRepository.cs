1c0002cf4e51b3c08366c30ea77b73e510d22b76
Phuc Vo <duyphuc_forza@yahoo.com>
2015-03-09 18:13:06 +0700

    Update source

---
+++ Data/EntityRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Data
+{
+    public class EntityRepository<TEntity> where TEntity: class
+    {
+        private DataContext context;
+        private DbSet<TEntity> dbSet;
+
+        public EntityRepository(DataContext dbContext)
+        {
+            this.context = dbContext;
+            this.dbSet = dbContext.Set<TEntity>();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IQueryable<TEntity> GetEntities(Expression<Func<TEntity, bool>> expression = null)
+        {
+            if (expression == null)
+            {
+                return dbSet;
+            }
+            return dbSet.Where(expression);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteById(object id)
+        {
+            TEntity entity = dbSet.Find(id);
+            Delete(entity);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UnitOfWork: IDisposable
+    {
+        private DataContext context;
+        private bool isDisposed = false;
+
+        public UnitOfWork()
+        {
+            this.isDisposed = false;
+            this.context = new DataContext();
+        }
+
+        public DataContext GetContext()
+        {
+            if (this.isDisposed || this.context == null)
+            {
+                this.context = new DataContext();
+                this.isDisposed = false;
+            }
+
+            return context;
+        }
+
+        #region IDisposable
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.isDisposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
