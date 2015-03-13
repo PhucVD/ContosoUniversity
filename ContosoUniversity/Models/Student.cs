c50440d5d0c0a21e7d0473cabdcf8e696d237152
Phuc Vo <duyphuc_forza@yahoo.com>
2015-03-13 19:27:36 +0700

    Update

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(20)]
         public string LastName { get; set; }
+
+        [StringLength(20, ErrorMessage="First name cannot be longer than 20 characters.")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
